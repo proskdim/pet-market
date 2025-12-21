d952a64bfdc8987a6f189ab8383987ed10f88645
proskurekov <proskurekovdim@gmail.com>
2025-12-21 11:40:38 +0700

    added searchProducts logic, addOne logic

--- apps/pet-market-be/src/app/products/products.service.ts
+++ apps/pet-market-be/src/app/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateProductInput } from './dto/create-product.input';
 import { UpdateProductInput } from './dto/update-product.input';
 import { PrismaService } from '../prisma/prisma.service';
+import { Product } from '../../../prisma/generated/prisma/client';
 
 @Injectable()
 export class ProductsService {
@@ -16,8 +17,25 @@ export class ProductsService {
     return this.prisma.product.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async searchProducts(term: string): Promise<Product[]> {
+    const lowerCaseTerm = term.toLowerCase();
+
+    return this.prisma.product.findMany({
+      where: {
+        OR: [
+          { name: { contains: lowerCaseTerm, mode: 'insensitive' } },
+          { description: { contains: lowerCaseTerm, mode: 'insensitive' } },
+        ]
+      }
+    });
+  }
+
+  findOne(id: string) {
+    return this.prisma.product.findUnique({
+      where: {
+        id: id
+      }
+    });
   }
 
   update(id: number, updateProductInput: UpdateProductInput) {
