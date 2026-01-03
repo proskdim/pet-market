104e7d8dbca2028ceea3744d7bdd9a530661815e
proskurekov <proskurekovdim@gmail.com>
2026-01-04 00:12:09 +0700

    fix: update GraphQL schema file path to use __dirname for better compatibility

--- apps/pet-market-be/src/app/app.module.ts
+++ apps/pet-market-be/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HealthModule } from './health/health.module';
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'apps/pet-market-be/dist/schema.gql'),
+      autoSchemaFile: join(__dirname, 'schema.gql'),
       graphiql: true,
     }),
     ProductsModule,
