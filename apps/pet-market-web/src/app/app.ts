23494074f476587c71688b3cbbc1b6de0b8f7423
proskurekov <proskurekovdim@gmail.com>
2026-01-03 19:04:54 +0700

    add header component and update app layout, add products component

--- apps/pet-market-web/src/app/app.ts
+++ apps/pet-market-web/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { Header } from './components/header/header';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, Header],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
